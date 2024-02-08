91660845252aa46853460dcce3c68c80785b69ac
Florian BOISSET <florian.boisset31@gmail.com>
2024-02-08 13:46:34 +0100

    add prefabs

--- Assets/Script/transitionEchelle.cs
+++ Assets/Script/transitionEchelle.cs
@@ -7,9 +7,9 @@ public class transitionEchelle : MonoBehaviour
 {
     public InputActionReference toggle;
 
-    public GameObject obj1;
-    public GameObject obj2;
-    public GameObject obj3;
+    public GameObject[] arr = new GameObject[3];
+
+
     public float scaleCoef = 100f;
     public GameObject _mainDroite;
 
@@ -20,40 +20,45 @@ public class transitionEchelle : MonoBehaviour
     void Start()
     {
         _mainDroite = GameObject.Find("Right Controller");
-        obj1 = GameObject.CreatePrimitive(PrimitiveType.Cube);
-        obj1.GetComponent<Transform>().localScale = 0.01f * Vector3.one;
-        obj1.GetComponent<Transform>().SetParent(_mainDroite.GetComponent<Transform>());
-        obj1.GetComponent<Transform>().localPosition = new Vector3(0, -0.3f, 0);
-        obj2 = GameObject.CreatePrimitive(PrimitiveType.Cube);
-        obj2.GetComponent<Transform>().localScale = scaleCoef * Vector3.one;
-        obj2.GetComponent<Transform>().localPosition = new Vector3(0, 50, 150);
+        arr[0].GetComponent<Transform>().localScale = 0.01f * Vector3.one;
+        arr[0].GetComponent<Transform>().SetParent(_mainDroite.GetComponent<Transform>());
+        arr[0].GetComponent<Transform>().localPosition = new Vector3(0, -0.3f, 0);
 
-        obj3 = GameObject.CreatePrimitive(PrimitiveType.Cube);
-        obj3.GetComponent<Transform>().localScale = 100f * Vector3.one;
-        obj3.SetActive(false);
+        arr[1] = GameObject.CreatePrimitive(PrimitiveType.Cube);
+        arr[1].GetComponent<Transform>().localScale = scaleCoef * Vector3.one;
+        arr[1].GetComponent<Transform>().localPosition = new Vector3(0, 50, 150);
+
+        arr[2] = GameObject.CreatePrimitive(PrimitiveType.Cube);
+        arr[2].GetComponent<Transform>().localScale = 100f * Vector3.one;
+        arr[2].SetActive(false);
+   
     }
 
     // Update is called once per frame
     void Update()
     {
+        
         toggle.action.started += Toggle;
         if(t)
         {
-            obj2.GetComponent<Transform>().position = Vector3.MoveTowards(obj2.transform.position, _mainDroite.GetComponent<Transform>().position, 5f);
+            arr[1].GetComponent<Transform>().position = Vector3.MoveTowards(arr[1].transform.position, _mainDroite.GetComponent<Transform>().position, 5f);
             if(scaleCoef > 0.1)
             {
                 scaleCoef /= 1.2f;
-                obj2.GetComponent<Transform>().localScale = scaleCoef * Vector3.one;
+                arr[1].GetComponent<Transform>().localScale = scaleCoef * Vector3.one;
             }
         }
+        
     }
 
     public void Toggle(InputAction.CallbackContext context)
     {
-        Destroy(obj1);
+        
+        Destroy(arr[0]);
         t = true;
-        obj2.GetComponent<Transform>().SetParent(_mainDroite.GetComponent<Transform>());
-        obj2.GetComponent<MeshRenderer>().material.color = Color.red;
+        arr[1].GetComponent<Transform>().SetParent(_mainDroite.GetComponent<Transform>());
+        arr[1].GetComponent<MeshRenderer>().material.color = Color.red;
+        
     }
 
 }
